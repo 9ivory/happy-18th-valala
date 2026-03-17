2cc7db82e7b125b76d56cd6f49880c9e8498f5c7
9ivory <yurei.chizuyo@gmail.com>
2026-03-16 21:06:36 -0700

    Add files via upload
    uhh start

---
+++ script.js
@@ -0,0 +1,12 @@
+const messages = [
+  "You are one of the kindest people I know.",
+  "Your generosity always stands out.",
+  "You’re always there when it matters most.",
+  "You’re genuinely so intelligent.",
+  "You make everything more fun.",
+  "Happy 18th! 🎉 You deserve everything good."
+];
+
+function showText(index) {
+  document.getElementById("letterText").innerText = messages[index];
+}
